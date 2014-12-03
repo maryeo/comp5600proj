0f627446d8c146e00d0730ed8a12ca3f3cbaedab
Mary Odum <maryeo@ymail.com>
2014-12-02 18:48:29 -0600

    Fixed an error

--- Agent.java
+++ Agent.java
@@ -1,170 +1,170 @@
-   import java.util.Hashtable;
-   import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.ArrayList;
 
-   public class Agent
+public class Agent
+{
+   int playerNum;
+   boolean andOr;
+
+   public Agent(int playerNumber, boolean useAndOr)
    {
-      int playerNum;
-      boolean andOr;
-   
-      public Agent(int playerNumber, boolean useAndOr)
+      playerNum = playerNumber;
+      andOr = useAndOr;
+   }
+
+   public int getMove(GameBoard state, int ply)
+   {
+      if (andOr)
       {
-         playerNum = playerNumber;
-         andOr = useAndOr;
+         return andOrSearch(state);
       }
-   
-      public int getMove(GameBoard state, int ply)
+      else
       {
-         if (andOr)
+         return alphaBetaSearch(state, ply);
+      }
+   }
+
+   public int getPlayerNum()
+   {
+      return playerNum;
+   }
+   
+   private int andOrSearch(GameBoard state)
+   {
+      int action = -1;
+   	
+      return action;
+   }
+
+   private int alphaBetaSearch(GameBoard state, int ply)
+   {
+      int v = Integer.MIN_VALUE;
+      int a = -1;
+      int alpha = Integer.MIN_VALUE;
+      int beta = Integer.MAX_VALUE;
+      Hashtable<GameBoard, Integer> hash = new Hashtable<GameBoard, Integer>();
+   
+      int count = 0;
+      for (int action : orderMoves(state, true))
+      {
+         GameBoard newState = result(state, action);
+         int newV = min(newState, alpha, beta, ply, count, hash);
+         if (newV > v)
          {
-            return andOrSearch(state);
+            v = newV;
+            a = action;
          }
-         else
+         if (v >= beta)
          {
-            return alphaBetaSearch(state, ply);
+            return a;
+         }
+         else if (v > alpha)
+         {
+            alpha = v;
          }
       }
    
-      public int getPlayerNum()
+      return a;
+   }
+
+   private int min(GameBoard state, int alpha, int beta, int ply, int count, Hashtable<GameBoard, Integer> hash)
+   {
+      Integer v = hash.get(state);
+      if (v == null)
       {
-         return playerNum;
+         if ((state.isGameOver() == -1) || count >= ply)
+         {
+            return H1(state);
+         }
+         v = Integer.MAX_VALUE;
+         for (int action : orderMoves(state, false))
+         {
+            GameBoard newState = result(state, action);
+            int newV = max(newState, alpha, beta, ply, count + 1, hash);
+            if (newV < v)
+            {
+               v = newV;
+            }
+            if (v <= alpha)
+            {
+               return v;
+            }
+            else if (v < beta)
+            {
+               beta = v;
+            }
+         }
+         hash.put(state, v);
       }
-      
-      private int andOrSearch(GameBoard state)
+      return v;
+   }
+
+   private int max(GameBoard state, int alpha, int beta, int ply, int count, Hashtable<GameBoard, Integer> hash)
+   {
+      Integer v = hash.get(state);
+      if (v == null)
       {
-         int action = -1;
-      	
-         return action;
-      }
-   
-      private int alphaBetaSearch(GameBoard state, int ply)
-      {
-         int v = Integer.MIN_VALUE;
-         int a = -1;
-         int alpha = Integer.MIN_VALUE;
-         int beta = Integer.MAX_VALUE;
-         Hashtable<GameBoard, Integer> hash = new Hashtable<GameBoard, Integer>();
-      
-         int count = 0;
+         if ((state.isGameOver()== -1)|| count >= ply)
+         {
+            return H1(state);
+         }
+         v = Integer.MIN_VALUE;
          for (int action : orderMoves(state, true))
          {
             GameBoard newState = result(state, action);
-            int newV = min(newState, alpha, beta, ply, count, hash);
+            int newV = min(newState, alpha, beta, ply, count + 1, hash);
             if (newV > v)
             {
                v = newV;
-               a = action;
             }
             if (v >= beta)
             {
-               return a;
+               return v;
             }
             else if (v > alpha)
             {
                alpha = v;
             }
          }
-      
-         return a;
+         hash.put(state, v);
       }
-   
-      private int min(GameBoard state, int alpha, int beta, int ply, int count, Hashtable<GameBoard, Integer> hash)
-      {
-         Integer v = hash.get(state);
-         if (v == null)
-         {
-            if (state.isGameOver()|| count >= ply)
-            {
-               return H1(state);
-            }
-            v = Integer.MAX_VALUE;
-            for (int action : orderMoves(state, false))
-            {
-               GameBoard newState = result(state, action);
-               int newV = max(newState, alpha, beta, ply, count + 1, hash);
-               if (newV < v)
-               {
-                  v = newV;
-               }
-               if (v <= alpha)
-               {
-                  return v;
-               }
-               else if (v < beta)
-               {
-                  beta = v;
-               }
-            }
-            hash.put(state, v);
-         }
-         return v;
-      }
-   
-      private int max(GameBoard state, int alpha, int beta, int ply, int count, Hashtable<GameBoard, Integer> hash)
-      {
-         Integer v = hash.get(state);
-         if (v == null)
-         {
-            if (state.isGameOver()|| count >= ply)
-            {
-               return H1(state);
-            }
-            v = Integer.MIN_VALUE;
-            for (int action : orderMoves(state, true))
-            {
-               GameBoard newState = result(state, action);
-               int newV = min(newState, alpha, beta, ply, count + 1, hash);
-               if (newV > v)
-               {
-                  v = newV;
-               }
-               if (v >= beta)
-               {
-                  return v;
-               }
-               else if (v > alpha)
-               {
-                  alpha = v;
-               }
-            }
-            hash.put(state, v);
-         }
-         return v;
-      }
-   
-      private GameBoard result(GameBoard state, int action)
-      {
-         state.updateGame(playerNum, action);
-         return state;
-      }
-   
-      private ArrayList<Integer> orderMoves(GameBoard state, boolean agentsMove)
-      {
-         ArrayList<Integer> moves = new ArrayList<Integer>();
-      
-         return moves;
-      }
-      
-      private ArrayList<Integer> actions(GameBoard state)
-      {
-         ArrayList<Integer> moves = new ArrayList<Integer>();
-         for (int i = 0; i < state.size(); i++)
-         {
-            if (state.get(playerNum, i) != 0)
-            {
-               moves.add(i);
-            }
-         }
-         return moves;
-      }
-   
-      private int H1(GameBoard state)
-      {
-         int result = 0;
-         int index = (playerNum + 1) % 2;
-         for (int i = 0; i < state.size(); i++)
-         {
-            result = result + state.get(index, i);
-         }
-         return result;
-      }
+      return v;
    }
+
+   private GameBoard result(GameBoard state, int action)
+   {
+      state.updateGame(playerNum, action);
+      return state;
+   }
+
+   private ArrayList<Integer> orderMoves(GameBoard state, boolean agentsMove)
+   {
+      ArrayList<Integer> moves = new ArrayList<Integer>();
+   
+      return moves;
+   }
+   
+   private ArrayList<Integer> actions(GameBoard state)
+   {
+      ArrayList<Integer> moves = new ArrayList<Integer>();
+      for (int i = 0; i < state.size(); i++)
+      {
+         if (state.get(playerNum, i) != 0)
+         {
+            moves.add(i);
+         }
+      }
+      return moves;
+   }
+
+   private int H1(GameBoard state)
+   {
+      int result = 0;
+      int index = (playerNum + 1) % 2;
+      for (int i = 0; i < state.size(); i++)
+      {
+         result = result + state.get(index, i);
+      }
+      return result;
+   }
+}
