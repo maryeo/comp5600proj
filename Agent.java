7a5d7370da7a707f5eef0698c3e7357081c27b01
Mordio1344 <rjs0022@auburn.edu>
2014-12-07 17:40:17 -0600

    Worked on and or. Doesn't work yet.

--- Agent.java
+++ Agent.java
@@ -1,10 +1,12 @@
    import java.util.Hashtable;
    import java.util.ArrayList;
+	import java.util.Random;
 
    public class Agent
    {
       int playerNum;
       boolean andOr;
+		Hashtable<GameBoard, Integer> prevMoves = new Hashtable<GameBoard, Integer>();
       
       public Agent()
       {
@@ -21,11 +23,13 @@
       {
          if (andOr)
          {
-            return andOrSearch(state);
+            return andOrSearch(state, ply);
          }
          else
          {
-            return alphaBetaSearch(state, ply);
+				int move = alphaBetaSearch(state, ply);
+				prevMoves.put(state, move);
+            return move;
          }
       }
    
@@ -34,40 +38,90 @@
          return playerNum;
       }
       
-      private int andOrSearch(GameBoard state)
+		//Adaptation of Figure 11 in chapter four.
+		//Recalculating plan every move. More costly, but should have better performance.
+      private int andOrSearch(GameBoard state, int ply)
       {
-         int action = -1;
-      	
+			//Instructions say don't use the move ordering for this algorithm!
+         int action = orSearch(state, new Hashtable<GameBoard, Integer>(), ply, 0).get(0);
          return action;
       }
+		
+		//Don't need to keep track of plan since 
+		private ArrayList<Integer> orSearch(GameBoard state, Hashtable<GameBoard, Integer> path, int ply, int count)
+		{
+			ArrayList<Integer> plan = new ArrayList<Integer>();
+			if(state.isGameOver() == playerNum)
+			{
+				plan.add(Integer.MAX_VALUE);
+				return plan;
+			}
+			if(path.containsKey(state))
+			{
+				return null;
+			}
+			if(count >= ply)
+			{
+				plan.add(H1(state));
+				return plan;
+			}
+			for(int action : actions(state))
+			{
+				path.put(state, action);
+				plan = andSearch(result(state, action), path, ply, count + 1);
+				if(plan != null)
+				{
+					return plan;
+				}
+			}
+			return null;
+		} 
+		
+		private ArrayList<Integer> andSearch(GameBoard state, Hashtable<GameBoard, Integer> path, int ply, int count)
+		{
+			if(state.isGameOver() == playerNum)
+			{
+			}
+			return null;
+		}
    
       private int alphaBetaSearch(GameBoard state, int ply)
       {
-         int v = Integer.MIN_VALUE;
-         int a = 0;
-         int alpha = Integer.MIN_VALUE;
-         int beta = Integer.MAX_VALUE;
-         Hashtable<GameBoard, Integer> hash = new Hashtable<GameBoard, Integer>();
-      
-         int count = 0;
-         for (int action : orderMoves(state, true))
-         {
-            GameBoard newState = result(state, action);
-            int newV = min(newState, alpha, beta, ply, count, hash);
-            if (newV > v)
-            {
-               v = newV;
-               a = action;
-            }
-            if (v >= beta)
-            {
-               return a;
-            }
-            else if (v > alpha)
-            {
-               alpha = v;
-            }
-         }
+		   int a = 0;
+			if(prevMoves.containsKey(state))
+			{
+				ArrayList<Integer> actions = actions(state);
+				Random rand = new Random();
+				a = rand.nextInt(actions.size());
+			}
+			else
+			{
+				int v = Integer.MIN_VALUE;
+         	int alpha = Integer.MIN_VALUE;
+         	int beta = Integer.MAX_VALUE;
+         	Hashtable<GameBoard, Integer> hash = new Hashtable<GameBoard, Integer>();
+      		
+         	int count = 0;
+         	for (int action : orderMoves(state, true))
+         	{
+            	GameBoard newState = result(state, action);
+            	int newV = min(newState, alpha, beta, ply, count, hash);
+            	if (newV > v)
+            	{
+               	v = newV;
+               	a = action;
+            	}
+            	if (v >= beta)
+            	{
+               	return a;
+            	}
+            	else if (v > alpha)
+            	{
+               	alpha = v;
+            	}
+         	}
+			}
+			
          return a;
       }
    
@@ -187,7 +241,7 @@
       private int H1(GameBoard state)
       {
          int result = 0;
-         int index = (playerNum + 1) % 2;
+         int index = (playerNum) % 2;
          for (int i = 0; i < state.size(); i++)
          {
             result = result + state.get(index, i);
